0c08ac0f38ddabbd280fb09c5f226b1fb7e0bd51
Daniel Ojeda <211@holbertonschool.com>
2018-09-15 03:49:31 +0000

    testing removing a line from cal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if len(sys.argv) is not 4:
+    if len(sys.argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         sys.exit(1)
 
