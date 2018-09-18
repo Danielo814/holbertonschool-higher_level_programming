bbec3fcfe0949eeca7fc8f3e2548a6b5aee1debf
Daniel Ojeda <211@holbertonschool.com>
2018-09-18 23:14:31 +0000

    adding a check to see if list is empty

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     retval = my_list[0]
-    if my_list is None or len(my_list) < 1:
+    if my_list is None or my_list is []:
         return None
     for i in my_list:
         if i > retval:
