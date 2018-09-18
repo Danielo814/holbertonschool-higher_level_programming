132bed88d96a98f2c05ddbad0c51b0ef99c92eac
Daniel Ojeda <211@holbertonschool.com>
2018-09-18 23:18:22 +0000

    add else statement

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,8 @@ def max_integer(my_list=[]):
     retval = my_list[0]
     if my_list is None or my_list is []:
         return None
-    for i in my_list:
-        if i > retval:
-            retval = i
-    return retval
+    else:
+        for i in my_list:
+            if i > retval:
+                retval = i
+        return retval
