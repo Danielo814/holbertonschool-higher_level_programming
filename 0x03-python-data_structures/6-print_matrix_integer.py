c11242cd3506eba9639991e1ac2ce5e10057f581
Daniel Ojeda <211@holbertonschool.com>
2018-09-18 20:16:49 +0000

    fixing str.format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in range(len(row)):
             if i == len(row) - 1:
-                print("{}".format(row[i]))
+                print("{:d}".format(row[i]))
             else:
-                print("{}".format(row[i]), end=" ")
+                print("{:d}".format(row[i]), end=" ")
