6d325d5ecbf9115e6a0aad409b82429f301fbc58
Daniel Ojeda <211@holbertonschool.com>
2018-09-18 16:47:06 +0000

    returns an edited copy of the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    if idx < 0:
+        return new_list
+    if idx > len(new_list) - 1:
+        return new_list
+    else:
+        new_list[idx] = element
+        return new_list
+
+    
