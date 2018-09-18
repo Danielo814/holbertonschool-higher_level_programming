0e7b6df3a93251247762c35cd795570707183231
Daniel Ojeda <211@holbertonschool.com>
2018-09-18 23:23:53 +0000

    fixing empty list case

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    retval = my_list[0]
-    if my_list is None or my_list is []:
+    if my_list is None or len(my_list) < 1:
         return None
     else:
+        retval = my_list[0]
         for i in my_list:
             if i > retval:
                 retval = i
