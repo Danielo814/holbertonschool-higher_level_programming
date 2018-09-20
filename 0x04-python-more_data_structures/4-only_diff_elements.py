b87d8b28109fcd8f3de631ae3c0c2b6e7192e263
Daniel Ojeda <211@holbertonschool.com>
2018-09-20 22:39:14 +0000

    returns elements only present in one set

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new_list = list(set_1)
-    new_list += list(set_2)
-    return set(new_list)
+    new = []
+    for i in set_2:
+        if i not in set_1:
+            new.append(i)
+    for j in set_1:
+        if j not in new and j not in set_2:
+            new.append(j)
+    return new
