6ee84059fd7037a8d0dbea95d642ed1c26240a92
Daniel Ojeda <211@holbertonschool.com>
2018-09-21 20:45:05 +0000

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    retval = 0
+    my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for i in roman_string:
+        if i in my_dict:
+            retval += my_dict[i]
+    for compare in range(0, len(roman_string) - 1):
+        if my_dict[roman_string[compare]] < my_dict[roman_string[compare + 1]]:
+            retval -= 2
+    return retval
