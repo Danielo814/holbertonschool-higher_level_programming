0fceefa12216d668dc1ad3bf86d8b8694c532e6e
Daniel Ojeda <211@holbertonschool.com>
2018-09-21 01:04:58 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    temp = 0
+    empty = ""
     if a_dictionary is None:
         return None
-    return max(a_dictionary.keys(), key=(lambda k: a_dictionary[k]))
+    for k, v in a_dictionary.items():
+        if a_dictionary[k] > temp:
+            temp = a_dictionary[k]
+            empty = k
+    return empty
+
