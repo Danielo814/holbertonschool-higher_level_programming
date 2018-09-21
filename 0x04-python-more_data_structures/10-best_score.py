8a73f27651eea44e2957a58417eb4cd1e8ee52c8
Daniel Ojeda <211@holbertonschool.com>
2018-09-21 15:48:12 +0000

    fix syntax

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     temp = 0
     empty = ""
-    if a_dictionary is None or if a_dictionary == {}:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     for k, v in a_dictionary.items():
         if a_dictionary[k] > temp:
