e2b4acb237704018f6f6306f184bacf224c06c15
Daniel Ojeda <211@holbertonschool.com>
2018-09-21 15:46:33 +0000

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     temp = 0
     empty = ""
-    if a_dictionary is None:
+    if a_dictionary is None or if a_dictionary == {}:
         return None
     for k, v in a_dictionary.items():
         if a_dictionary[k] > temp:
