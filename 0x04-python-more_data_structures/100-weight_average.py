4c852d6e19907efe437ae7cb24b635a4b1020f7b
Daniel Ojeda <211@holbertonschool.com>
2018-09-26 02:02:41 +0000

    weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    retval = 0
+    temp = 0
+    for i in range(0, len(my_list)):
+        for j in range(0, 1):
+            retval += my_list[i][j] * my_list[i][j + 1]
+            temp += my_list[i][j + 1]
+    retval /= temp
+    return retval
