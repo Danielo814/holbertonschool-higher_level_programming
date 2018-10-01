581f4c8d38c88f6e2602afd42d36138819074964
Daniel Ojeda <211@holbertonschool.com>
2018-10-01 21:52:40 +0000

    divides 2 integers and prints result

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        retval = a / b
+    except ZeroDivisionError:
+        retval = None
+    finally:
+        print("Inside result: {}".format(retval))
