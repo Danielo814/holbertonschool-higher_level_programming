0113d46f679f4d78b68c689ba94c6f1c20bc0e18
Daniel Ojeda <211@holbertonschool.com>
2018-10-02 22:59:59 +0000

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Square class
+"""
+
+
+class Square:
+    """
+    square private instance attritute size
+    """
+    def __init__(self, size):
+        """
+        instantiation with size
+        """
+        self.__size = size
