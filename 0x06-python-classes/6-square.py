d64a8dd0fc63b054c9d1a0d6dc9aaaaf08ddb0a0
Daniel Ojeda <211@holbertonschool.com>
2018-10-03 01:38:17 +0000

    add error checking

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -66,6 +66,8 @@ class Square:
         """
         sets a position of a square
         """
-        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
+        if type(value) is not tuple or value[0] < 0 or value[1] < 0 or \
+           type(value[0]) is not int or type(value[1]) is not int or \
+           len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
