39a13de3c8d5ea3763f01af35445b93ca862ad93
Daniel Ojeda <211@holbertonschool.com>
2018-10-03 00:14:16 +0000

    computes the area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Square class
+"""
+
+
+class Square:
+    """
+    square private instance attritute size
+    """
+    def __init__(self, size=0):
+        """
+        instantiation with size
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        returns the current square area
+        """
+        return self.__size * self.__size
