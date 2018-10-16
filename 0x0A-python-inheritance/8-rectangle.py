a87fd56ff61d4d1f26fab9c967e4f0545244d3ac
Daniel Ojeda <211@holbertonschool.com>
2018-10-16 00:59:52 +0000

    change order of init statements

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle(BaseGeometry):
         """
         initialize width and height
         """
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
+        self.__width = width
         self.integer_validator("height", height)
+        self.__height = height
