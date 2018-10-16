5d78c867aba8defad3554a4b6f8e7f88a24cbf45
Daniel Ojeda <211@holbertonschool.com>
2018-10-16 00:55:12 +0000

    print string formatted rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+9-rectangle module
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class Rectangle that inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        initialize width and height
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """
+        returns area
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        str method to print rectangle descriptions
+        """
+        return "[{}] {}/{}".format(self.__class__.__name__,
+                                   self.__width, self.__height)
