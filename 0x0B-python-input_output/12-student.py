70ad821b9fccacb13a05bf74c9371feccf7aa3d0
Daniel Ojeda <211@holbertonschool.com>
2018-10-16 22:10:20 +0000

    update student class

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+12-student module
+"""
+
+
+class Student:
+    """
+    class student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initialize Student attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retreives a dictionary representation of instance
+        """
+        new = {}
+        if attrs is None:
+            return self.__dict__
+        else:
+            for i in attrs:
+                if i in self.__dict__:
+                    new[i] = self.__dict__[i]
+                else:
+                    continue
+        return new
