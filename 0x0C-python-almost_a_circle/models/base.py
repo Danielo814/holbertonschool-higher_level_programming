0333d15be3caebca508c12f77a961e0e70a203db
Daniel Ojeda <211@holbertonschool.com>
2018-10-19 18:26:49 +0000

    changes to base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
 base module
 """
+import json
 
 
 class Base:
@@ -19,3 +20,13 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of
+        list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
