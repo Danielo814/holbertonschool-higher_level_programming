17c9cae6464d46d6087926cd451a627f324882c0
Daniel Ojeda <211@holbertonschool.com>
2018-10-19 18:23:19 +0000

    rectangle class update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -31,20 +31,28 @@ class Rectangle(Base):
         """
         set width
         """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
     def height(self):
-        """                                                                    
-        getter for width                                                       
+        """
+        getter for width
         """
         return self.__height
 
-    @width.setter
+    @height.setter
     def height(self, value):
-        """                                                                    
-        set width                                                              
         """
+        set width
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -59,6 +67,10 @@ class Rectangle(Base):
         """
         setter for x
         """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -73,4 +85,79 @@ class Rectangle(Base):
         """
         setter for y
         """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """
+        returns the area value of a rectangle instance
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """
+        prints a rectangle with the character '#'
+        """
+        print('\n' * self.__y, end="")
+        for row in range(self.__height):
+            print(' ' * self.__x, end="")
+            for column in range(self.__width):
+                if column != self.__width - 1:
+                    print('#', end="")
+                else:
+                    print('#')
+
+    def __str__(self):
+        """
+        override __str__ method to print nicely formatted string
+        """
+        return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
+                                                self.id,
+                                                self.__x,
+                                                self.__y,
+                                                self.__width,
+                                                self.__height)
+
+    def update(self, *args, **kwargs):
+        """
+        assigns an argument to each attribute
+        """
+        if args:
+            for arg in range(len(args)):
+                if arg == 0:
+                    self.id = args[arg]
+                if arg == 1:
+                    self.__width = args[arg]
+                if arg == 2:
+                    self.__height = args[arg]
+                if arg == 3:
+                    self.__x = args[arg]
+                if arg == 4:
+                    self.__y = args[arg]
+
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'width' in kwargs:
+                self.width = kwargs['width']
+            if 'height' in kwargs:
+                self.height = kwargs['height']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
+
+    def to_dictionary(self):
+        """
+        returns the dictionary representation of a Rectangle
+        """
+        new_dict = {}
+        new_dict['id'] = self.id
+        new_dict['width'] = self.width
+        new_dict['height'] = self.height
+        new_dict['x'] = self.x
+        new_dict['y'] = self.y
+        return new_dict
