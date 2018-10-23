532dc2b05ea12ea5ce36d989d5f8fc4ac83aa2b1
Daniel Ojeda <211@holbertonschool.com>
2018-10-23 20:57:23 +0000

    fix docstring

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """
-    Rectangle class
+    Rectangle class, inherits from base
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """
