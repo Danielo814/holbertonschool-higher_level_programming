6949210d46527a9e6e96136993c3de2f368e519a
Daniel Ojeda <211@holbertonschool.com>
2018-12-04 22:28:14 +0000

    create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- create server user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* to 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
