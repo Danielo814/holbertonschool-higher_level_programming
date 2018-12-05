79a1beb6a247faa94bf2d15689e96cd5e19b2a44
Daniel Ojeda <211@holbertonschool.com>
2018-12-05 02:27:40 +0000

    create a database, and a table in the database

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- creates a database hbtn_0d_usa and a table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT PRIMARY KEY AUTO_INCREMENT NOT NULL, name VARCHAR(256) NOT NULL);
