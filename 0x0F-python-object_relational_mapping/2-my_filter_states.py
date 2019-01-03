8af1877ef47ae77069f92d815732bec3d4a22e6a
Daniel Ojeda <211@holbertonschool.com>
2019-01-03 21:04:37 +0000

    changing to create SQL query with user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id asc")
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id asc"
+                .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         if row[1] == sys.argv[4]:
