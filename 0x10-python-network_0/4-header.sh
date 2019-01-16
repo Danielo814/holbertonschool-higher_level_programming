07062a3e9f4f70833b73d2fbd9fd5612648a3235
Daniel Ojeda <211@holbertonschool.com>
2019-01-16 01:53:41 +0000

    change order of script

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes a URL as an argument, sends GET request, and displays the body of the response
-curl -sX GET -H $1 "X-HolbertonSchool-User-Id: 98"
+curl -sX GET -H "X-HolbertonSchool-User-Id: 98" $1
