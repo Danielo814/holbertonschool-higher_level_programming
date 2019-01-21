29cd46adc7ec2ac407d798aa69557693b37a9875
Daniel Ojeda <211@holbertonschool.com>
2019-01-21 22:37:17 +0000

    fix spelling mistake

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,6 +9,6 @@ import sys
 if __name__ == "__main__":
     req = requests.get(sys.argv[1])
     if req.status_code >= 400:
-        print("Error Code: {}".format(req.status_code))
+        print("Error code: {}".format(req.status_code))
     else:
         print(req.text)
