0716e4b2a5986711f7048e0b8ec27ba30e11d10b
Daniel Ojeda <211@holbertonschool.com>
2019-01-21 22:32:18 +0000

    error code checking

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+takes in a url and does error status code checking
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+    if req.status_code >= 400:
+        print("Error Code: {}".format(req.status_code))
+    else:
+        print(req.text)
